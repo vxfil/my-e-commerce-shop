422e50845c7f9e46df8d89c0e47fb22d2b68a2fc
Vladyslav Fylymonov <fylymonov.vladyslav@gmail.com>
2023-07-23 18:19:02 +0300

    added redux/redux-toolkit

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom';
-// import 'bootstrap/dist/css/bootstrap.min.css';
+import { Provider } from 'react-redux';
 import './assets/styles/bootstrap.custom.css'
 import './assets/styles/index.css';
 import App from './App';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
 import reportWebVitals from './reportWebVitals';
+import store from './store';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -21,7 +22,9 @@ const router = createBrowserRouter(
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
   </React.StrictMode>
 );
 
--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,23 +1,29 @@
 import React from 'react'
 import { Row, Col } from 'react-bootstrap';
 import Product from '../components/Product';
-import products from '../products';
+import { useGetProductsQuery } from '../slices/productsApiSlice';
 
 const HomeScreen = () => {
+    const { data: products, isLoading, error } = useGetProductsQuery();
+
     return (
         <>
-            <h1>Latest Products</h1>
-            <Row>
-                {
-                    products.map((product) => (
-                        <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-                            <Product product={product} />
-                        </Col>
-                    ))
-                }
-            </Row>
+            {isLoading ? (<h2>Loading...</h2>) : error ? (<div>{error?.data?.message || error.error}</div>) : (
+                <>
+                    <h1>Latest Products</h1>
+                    <Row>
+                        {
+                            products.map((product) => (
+                                <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+                                    <Product product={product} />
+                                </Col>
+                            ))
+                        }
+                    </Row>
+                </>
+            )}
         </>
     )
 }
 
-export default HomeScreen
+export default HomeScreen;
