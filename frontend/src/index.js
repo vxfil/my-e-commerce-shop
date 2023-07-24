4f5029c4211a71f0011e41aa5fdca17b3dfadd73
Vladyslav Fylymonov <fylymonov.vladyslav@gmail.com>
2023-07-24 16:24:34 +0300

    added cart screen

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,6 +7,7 @@ import './assets/styles/index.css';
 import App from './App';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
+import CartScreen from './screens/CartScreen';
 import reportWebVitals from './reportWebVitals';
 import store from './store';
 
@@ -15,6 +16,7 @@ const router = createBrowserRouter(
     <Route path="/" element={<App />}>
       <Route index={true} path='/' element={<HomeScreen />} />
       <Route path='/product/:id' element={<ProductScreen />} />
+      <Route path='/cart' element={<CartScreen />} />
     </Route>
   )
 );
