30952b235a8efb3b6b01559db5d06ccba2f4e598
Vladyslav Fylymonov <fylymonov.vladyslav@gmail.com>
2023-07-30 16:59:58 +0300

    added payment screen

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -12,6 +12,7 @@ import LoginScreen from './screens/LoginScreen';
 import RegisterScreen from './screens/RegisterScreen';
 import ShippingScreen from './screens/ShippingScreen';
 import PrivateRoute from './components/PrivateRoute';
+import PaymentScreen from './screens/PaymentScreen';
 import reportWebVitals from './reportWebVitals';
 import store from './store';
 
@@ -26,6 +27,7 @@ const router = createBrowserRouter(
 
       <Route path='' element={<PrivateRoute />}>
         <Route path='/shipping' element={<ShippingScreen />} />
+        <Route path='/payment' element={<PaymentScreen />} />
       </Route>
     </Route>
   )
